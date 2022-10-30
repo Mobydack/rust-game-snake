df09648aa88ec23069d1cf071f09262ad9bf10fa
Mobydack <devzetsubou@gmail.com>
2022-10-31 02:37:01 +0300

    feat: support infinity field and time iteration

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 mod direction_state;
 mod snake;
 
-use bevy::{
-    prelude::{App, Camera2dBundle, Commands},
-    DefaultPlugins,
-};
+use bevy::time::FixedTimestep;
+use bevy::{prelude::*, DefaultPlugins};
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera2dBundle::default());
@@ -15,7 +13,12 @@ fn main() {
         .add_startup_system(setup_camera)
         .add_startup_system(snake::spawn)
         .add_system(snake::direction_system)
-        .add_system(snake::position_system)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0))
+                .with_system(snake::position_iteration),
+        )
+        .add_system(snake::position_translation.after(snake::position_iteration))
         .add_plugins(DefaultPlugins)
         .run();
 }
--- src/snake.rs
+++ src/snake.rs
@@ -1,10 +1,7 @@
 use super::direction_state::{Direction, DirectionState};
-use bevy::{
-    math::Vec3,
-    prelude::{
-        default, Color, Commands, Component, Input, KeyCode, Query, Res, Sprite, SpriteBundle,
-        Transform,
-    },
+use bevy::prelude::{
+    default, Color, Commands, Component, Input, KeyCode, Query, Res, Sprite, SpriteBundle,
+    Transform, UVec2, Vec3, Windows,
 };
 
 pub const SNAKE_COLOR: Color = Color::rgb(0.0, 1.0, 0.0);
@@ -12,6 +9,7 @@ pub const SNAKE_COLOR: Color = Color::rgb(0.0, 1.0, 0.0);
 #[derive(Component)]
 pub struct SnakeHead {
     direction_state: DirectionState,
+    position: UVec2,
 }
 
 pub fn spawn(mut commands: Commands) {
@@ -21,14 +19,11 @@ pub fn spawn(mut commands: Commands) {
                 color: SNAKE_COLOR,
                 ..default()
             },
-            transform: Transform {
-                scale: Vec3::new(10.0, 10.0, 10.0),
-                ..default()
-            },
             ..default()
         })
         .insert(SnakeHead {
             direction_state: DirectionState::new(),
+            position: UVec2::new(10, 10),
         });
 }
 
@@ -49,21 +44,65 @@ pub fn direction_system(
     }
 }
 
-pub fn position_system(mut snake_position: Query<(&SnakeHead, &mut Transform)>) {
-    if let Some((snake_head, mut transform)) = snake_position.iter_mut().next() {
-        match snake_head.direction_state.get_value() {
-            Direction::Up => {
-                transform.translation.y += 2.;
-            }
-            Direction::Down => {
-                transform.translation.y -= 2.;
-            }
-            Direction::Right => {
-                transform.translation.x += 2.;
-            }
-            Direction::Left => {
-                transform.translation.x -= 2.;
-            }
+fn normalize_coord(current_value: isize, max_value: isize) -> u32 {
+    let mut result = current_value;
+
+    if current_value < 0 {
+        result = max_value - current_value;
+    } else if current_value > max_value {
+        result = current_value - max_value;
+    }
+
+    result as u32
+}
+
+fn normalize_number_sign(direction: Direction, value: isize) -> isize {
+    value
+        * match direction {
+            Direction::Down | Direction::Left => -1,
+            _ => 1,
         }
+}
+
+fn convert_position_to_translation(coord: f32, window_side_length: f32, grid_length: f32) -> f32 {
+    let cell_size = window_side_length / grid_length;
+    let cell_middle = cell_size / 2.;
+    let window_middle = window_side_length / 2.;
+
+    coord / grid_length * window_side_length - window_middle + cell_middle
+}
+
+pub fn position_iteration(mut snake_heads: Query<(&mut SnakeHead)>) {
+    if let Some(mut snake_head) = snake_heads.iter_mut().next() {
+        let current_direction = snake_head.direction_state.get_value();
+
+        snake_head.position = match current_direction {
+            Direction::Left | Direction::Right => UVec2::new(
+                normalize_coord(
+                    snake_head.position.x as isize + normalize_number_sign(current_direction, 1),
+                    10,
+                ),
+                snake_head.position.y,
+            ),
+            Direction::Up | Direction::Down => UVec2::new(
+                snake_head.position.x,
+                normalize_coord(
+                    snake_head.position.y as isize + normalize_number_sign(current_direction, 1),
+                    10,
+                ),
+            ),
+        }
+    }
+}
+
+pub fn position_translation(windows: Res<Windows>, mut query: Query<(&SnakeHead, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+
+    for (snake_head, mut transform) in query.iter_mut() {
+        transform.translation = Vec3::new(
+            convert_position_to_translation(snake_head.position.x as f32, window.width(), 10.),
+            convert_position_to_translation(snake_head.position.y as f32, window.height(), 10.),
+            0.,
+        );
     }
 }
